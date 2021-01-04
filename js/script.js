d6da0752d21027364b80779992732b1c6eb96d73
serenapascual <serena.a.pascual@gmail.com>
2021-01-03 16:49:34 -0800

    Revert "Fix imports"
    This reverts commit 41e65a536c5e84439ce945fb9ee8251b4a809d8d.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import * as THREE from '../node_modules/three/src/Three.js';
+import * as THREE from '../node_modules/three/build/three.module.js';
 import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
 import Swarm from './swarm.js';
 
